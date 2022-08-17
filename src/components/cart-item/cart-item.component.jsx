6ce9055e3c5392aeb77930efb91941489b3d40b8
Joseph Driscoll <driscoll.recording@gmail.com>
2022-08-17 15:10:15 -0400

    cart drop down styling

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,8 +1,9 @@
 
+import './cart-item.styles.scss';
 const CartItem = ({ cartItem }) => {
     const { name, quantity, imageUrl } = cartItem;
     return (
-        <div>
+        <div className='cart-item-container'>
             <img src={imageUrl} alt={`${name}`} />
             <h2>{name}</h2>
             <h4>{quantity}</h4>
